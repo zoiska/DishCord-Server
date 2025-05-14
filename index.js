4383e7c2932f94db3f8ade2fdae251e7f1c12e47
rhe <qbbqbbb@gmail.com>
2025-05-14 15:50:24 +0200

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
