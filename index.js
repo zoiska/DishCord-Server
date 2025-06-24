37002ed96f3639d1e5f542355d876c2ad14bb4f3
rhe <qbbqbbb@gmail.com>
2025-06-24 17:45:38 +0200

    changed image path

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(passport.initialize());
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
+  destination: (req, file, cb) => cb(null, "public/uploads/"),
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
   },
